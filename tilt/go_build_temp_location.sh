d4931e83fc165b18de1ec9fa236511bb7a333bb6
Matthew Hodgkins <matthodge@gmail.com>
2020-03-01 11:42:21 +0100

    Add Comments to Build File

--- tilt/go_build_temp_location.sh
+++ tilt/go_build_temp_location.sh
@@ -4,17 +4,22 @@ set -euo pipefail
 IFS=$'\n\t'
 
 # Builds a go binary in a temporary location and moves it into the apps/APPNAME/bin directory
-
 APPNAME=$1
 
 # Make temp directory
 tmpDir=$(mktemp -d)
 cd apps/$APPNAME
+
+# Export Build Options
 export CGO_ENABLED=0
 export GOARCH=amd64
 export GOOS=linux
+
+# Build the binary to a temp location
 echo "Building $APPNAME into $tmpDir"
 go build -a -installsuffix cgo -ldflags '-s -w' -o $tmpDir/$APPNAME
+
+# Move it into a place that Tilt is watching ("apps/<NAME>/bin")
 binDirectory=bin/
 echo "Clearing $binDirectory"
 rm -rf $binDirectory
